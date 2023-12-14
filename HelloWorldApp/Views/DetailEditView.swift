2bb79250e224db14c1121e902f46b10ea0313b48
Max Kiselyov <77746941+mkslv@users.noreply.github.com>
2023-12-14 22:02:42 +0000

    Scrumdinger AVFoundation

---
+++ HelloWorldApp/Views/DetailEditView.swift
@@ -0,0 +1,62 @@
+//
+//  DetailEditView.swift
+//  HelloWorldApp
+//
+//  Created by Max Kiselyov on 12/13/23.
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    @Binding var scrum: DailyScrum
+    @State private var newAttendeeName = ""
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Title", text: $scrum.title)
+                HStack {
+                    Slider(value: $scrum.lengthInMinutesAsDouble, in: 5...30, step: 1) {
+                        Text("Length")
+                    }
+                    .accessibilityValue("\(scrum.lengthInMinutes) minutes")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                        .accessibilityHidden(true)
+                }
+                ThemePicker(selection: $scrum.theme)
+            } header: {
+                Text("Meeting Info")
+            }
+            Section {
+                ForEach(scrum.attendees) { attendee in
+                    Text(attendee.name)
+                }
+                .onDelete { indices in
+                    scrum.attendees.remove(atOffsets: indices)
+                }
+                HStack {
+                    TextField("New Attendee", text: $newAttendeeName)
+                    Button(action: {
+                        withAnimation {
+                            let attendee = DailyScrum.Attendee(name: newAttendeeName)
+                            scrum.attendees.append(attendee)
+                            newAttendeeName = ""
+                        }
+                    }) {
+                        Image(systemName: "plus.circle.fill")
+                            .accessibilityLabel("Add attendee")
+                    }
+                    .disabled(newAttendeeName.isEmpty)
+                }
+            } header: {
+                Text("Attendees")
+            }
+            
+        }
+    }
+}
+
+#Preview {
+    DetailEditView(scrum: .constant(DailyScrum.sampleData[2]))
+}
